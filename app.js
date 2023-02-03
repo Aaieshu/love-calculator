f2082e53c1638fd66952c554205975045b6e7f16
aaie <120916094+Aaieshu@users.noreply.github.com>
2023-02-03 10:07:12 +0545

    project start gareko

---
+++ app.js
@@ -0,0 +1,39 @@
+// console.log("hello world");
+// alert("k chha?");
+// let response = prompt("what is your name");
+// alert("hello" + response);
+// let output = document.querySelector("#output");
+// console.log(output);
+// output.innerText = response;
+
+let yourName = document.querySelector("#yourName");
+let theirName = document.querySelector("#theirName");
+let output = document.querySelector("#output")
+let singlePeople = ['rupesh', 'shyam', 'hariS']
+
+function calculate() {
+    let name1 = yourName.value;
+    let name2 = theirName.value;
+    if (name1.length == 0 || name2.length == 0) {
+        output.innerText = "love sove nagari padhna ja!"
+
+
+    }
+    else {
+        if (singlePeople.includes(name1.toLowerCase())) {
+            output.innerText = " Single Forever";
+            yourName.value = ""
+            theirName.value = ""
+        }
+        else {
+
+
+            // console.log(name1, name2)
+            let lovepercent = parseInt(Math.random() * 100)
+
+            output.innerText = name1 + " & " + name2 + "  are  99.4% in love"
+            yourName.value = ""
+            theirName.value = ""
+        }
+    }
+}
